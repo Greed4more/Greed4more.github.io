3bb88b760663c787736349d1e399d3f4e1a0b501
Greed4more <shaunak2710@gmail.com>
2026-01-09 19:06:45 +0530

    push

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import path from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
-  base: "/Greed4more.github.io/",
+  base: "/Greed4more.github.io/", // 👈 MUST match repo name exactly
   plugins: [react()],
   resolve: {
     alias: {
